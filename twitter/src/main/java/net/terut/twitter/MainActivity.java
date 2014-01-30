7a233c3d009fb9fcd60299e2b747199b576348ff
terut <terut.dev+github@gmail.com>
2014-01-31 02:02:23 +0900

    Add refresh function

--- twitter/src/main/java/net/terut/twitter/MainActivity.java
+++ twitter/src/main/java/net/terut/twitter/MainActivity.java
@@ -67,7 +67,8 @@ public class MainActivity extends ListActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.menu_refresh) {
+            reloadTimeLine();
             return true;
         }
         return super.onOptionsItemSelected(item);
