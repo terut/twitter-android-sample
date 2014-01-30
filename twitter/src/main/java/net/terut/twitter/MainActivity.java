ec7253024c920465f4aeeff9b7acd81826308e5d
terut <terut.dev+github@gmail.com>
2014-01-31 01:05:34 +0900

    Show timeline with only text

--- twitter/src/main/java/net/terut/twitter/MainActivity.java
+++ twitter/src/main/java/net/terut/twitter/MainActivity.java
@@ -3,7 +3,10 @@ package net.terut.twitter;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -11,29 +14,40 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import net.terut.twitter.lib.TwitterClient;
 
-public class MainActivity extends Activity {
+import java.util.ArrayList;
+import java.util.List;
+
+import twitter4j.ResponseList;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+
+public class MainActivity extends ListActivity {
+
+    private TweetAdapter mAdapter;
+    private Twitter mTwitter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
 
         if (!TwitterClient.hasAccessToken(this)) {
             Intent intent = new Intent(this, TwitterOAuthActivity.class);
             startActivity(intent);
             finish();
+        } else {
+            mAdapter = new TweetAdapter(this);
+            setListAdapter(mAdapter);
+
+            mTwitter = TwitterClient.getTwitterInstance(this);
+            reloadTimeLine();
         }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -55,6 +69,43 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    private void reloadTimeLine() {
+        AsyncTask<Void, Void, List<String>> task = new AsyncTask<Void, Void, List<String>>() {
+            @Override
+            protected List<String> doInBackground(Void... args) {
+                try {
+                    ResponseList<twitter4j.Status> timeline = mTwitter.getHomeTimeline();
+                    ArrayList<String> list = new ArrayList<String>();
+                    for (twitter4j.Status status : timeline) {
+                        list.add(status.getText());
+                    }
+                    return list;
+                } catch (TwitterException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(List<String> result) {
+                if (result != null) {
+                    mAdapter.clear();
+                    for (String status : result) {
+                        mAdapter.add(status);
+                    }
+                    getListView().setSelection(0);
+                } else {
+                    showToast("タイムラインの取得に失敗しました");
+                }
+            }
+        };
+        task.execute();
+    }
+
+    private void showToast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+
     /**
      * A placeholder fragment containing a simple view.
      */
@@ -68,6 +119,12 @@ public class MainActivity extends Activity {
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
+        }
+    }
+
+    private class TweetAdapter extends ArrayAdapter<String> {
+        public TweetAdapter(Context context) {
+            super(context, android.R.layout.simple_list_item_1);
         }
     }
 
