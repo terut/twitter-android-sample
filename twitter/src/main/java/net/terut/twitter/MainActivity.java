f8447db5a450bfc4f18b24845f34de8f9f3762b2
terut <terut.dev+github@gmail.com>
2014-01-31 02:20:11 +0900

    Add tweet function

--- twitter/src/main/java/net/terut/twitter/MainActivity.java
+++ twitter/src/main/java/net/terut/twitter/MainActivity.java
@@ -66,11 +66,16 @@ public class MainActivity extends ListActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.menu_refresh) {
-            reloadTimeLine();
-            return true;
+        switch (item.getItemId()) {
+            case R.id.menu_refresh:
+                reloadTimeLine();
+                return true;
+            case R.id.menu_tweet:
+                Intent intent = new Intent(this, TweetActivity.class);
+                startActivity(intent);
+                return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 
---
+++ twitter/src/main/java/net/terut/twitter/TweetActivity.java
@@ -0,0 +1,66 @@
+package net.terut.twitter;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import net.terut.twitter.lib.TwitterClient;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+
+public class TweetActivity extends Activity {
+
+    private EditText mInputText;
+    private Twitter mTwitter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tweet);
+
+        mTwitter = TwitterClient.getTwitterInstance(this);
+
+        mInputText = (EditText) findViewById(R.id.input_text);
+
+        findViewById(R.id.action_tweet).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tweet();
+            }
+        });
+    }
+
+    private void tweet() {
+        AsyncTask<String, Void, Boolean> task = new AsyncTask<String, Void, Boolean>() {
+            @Override
+            protected Boolean doInBackground(String... args) {
+                try {
+                    mTwitter.updateStatus(args[0]);
+                    return true;
+                } catch (TwitterException e) {
+                    e.printStackTrace();
+                    return false;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(Boolean result) {
+                if (result) {
+                    showToast("ツイート完了");
+                    finish();
+                } else {
+                    showToast("ツイート失敗");
+                }
+            }
+        };
+        task.execute(mInputText.getText().toString());
+    }
+
+    private void showToast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+}
